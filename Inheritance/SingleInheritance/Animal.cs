6de1b3dc02b4c78ace129fd84121cd9b2096cc05
Mirela Mileva <mirela.tsvetanova2@gmail.com>
2020-10-28 00:31:32 +0000

    Add Inheritance Problem

---
+++ Inheritance/SingleInheritance/Animal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.PortableExecutable;
+using System.Text;
+
+namespace Farm
+{
+    public class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("eating...");
+        }
+    }
+}
