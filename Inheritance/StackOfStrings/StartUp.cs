cddd89c3c61416e493564c8d002009098e62dfda
Mirela Mileva <mirela.tsvetanova2@gmail.com>
2020-10-28 00:32:19 +0000

    Add Custom Stack

---
+++ Inheritance/StackOfStrings/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var myStack = new StackOfStrings();
+
+            myStack.AddRange(new List<string> { "Ana", "Mimi" });
+
+            Console.WriteLine(string.Join(" ", myStack));
+        }
+    }
+}
