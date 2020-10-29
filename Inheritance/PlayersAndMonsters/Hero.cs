ba14ae60f1dbd96c9ccd60adeae3f9c400c0ddc5
Mirela Mileva <mirela.tsvetanova2@gmail.com>
2020-10-29 00:40:13 +0000

    Add Players And Monsters Problem

---
+++ Inheritance/PlayersAndMonsters/Hero.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace PlayersAndMonsters
+{
+    public class Hero
+    {
+        private string username;
+        private int level;
+        public Hero(string username, int level)
+        {
+            this.Username = username;
+            this.Level = level;
+        }
+
+        public string Username 
+        { 
+            get
+            {
+                return this.username;
+            }
+            private set
+            {
+                this.username = value;
+            }
+        }
+        public int Level { get; set; }
+
+        public override string ToString()
+        {
+            return $"Type: {this.GetType().Name} Username: {this.Username} Level: {this.Level}";
+        }
+    }
+}
