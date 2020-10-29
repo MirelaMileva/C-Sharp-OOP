9dce6dfea5b2e10db957ae78681688bfcdbb9077
Mirela Mileva <mirela.tsvetanova2@gmail.com>
2020-10-29 00:37:18 +0000

    Add Zoo Problem

---
+++ Inheritance/Zoo/Lizard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Zoo
+{
+    public class Lizard : Reptile
+    {
+        public Lizard(string name)
+            : base(name)
+        {
+
+        }
+    }
+}
