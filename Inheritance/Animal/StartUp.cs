d3aab4d268386b179f167d148e0f6d83e63fbbaa
Mirela Mileva <mirela.tsvetanova2@gmail.com>
2020-10-31 17:55:58 +0000

    Add Animal Problem

---
+++ Inheritance/Animal/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using Animals;
+using System;
+
+namespace Animals
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Engine engine = new Engine();
+                engine.Run();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
