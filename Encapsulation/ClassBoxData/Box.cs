74bc10cb5b4ff6a7d2a41651539e82c74a7700cc
Mirela Mileva <mirela.tsvetanova2@gmail.com>
2020-11-01 00:43:39 +0000

    Add Class Box Data

---
+++ Encapsulation/ClassBoxData/Box.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace ClassBoxData
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Length 
+        { 
+            get
+            {
+                return this.length;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Length cannot be zero or negative.");
+                }
+
+                this.length = value;
+            }
+        }
+        public double Width
+        {
+            get
+            {
+                return this.width;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                }
+
+                this.width = value;
+            }
+        }
+        public double Height
+        {
+            get
+            {
+                return this.height;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Height cannot be zero or negative.");
+                }
+
+                this.height = value;
+            }
+        }
+
+        public double SurfaceArea()
+        {
+            double surfaceAreaSum = (2 * length * width) + (2 * length * height) + (2 * width * height);
+            return surfaceAreaSum;
+        }
+
+        public double LateralSurfaceArea()
+        {
+            double lateralArea = (2 * length * height) + (2 * width * height);
+            return lateralArea;
+        }
+
+        public double Volume()
+        {
+            double volumeSum = length * width * height;
+            return volumeSum;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Surface Area - {this.SurfaceArea():f2}");
+            sb.AppendLine($"Lateral Surface Area - {this.LateralSurfaceArea():f2}");
+            sb.AppendLine($"Volume - {this.Volume():f2}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Encapsulation/ClassBoxData/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ClassBoxData
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            Box box = null;
+
+            try
+            {
+                box = new Box(length, width, height);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine(box);
+
+        }
+    }
+}
