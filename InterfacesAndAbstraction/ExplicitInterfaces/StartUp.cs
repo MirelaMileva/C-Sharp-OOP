c041f6e51049e8d72cf02973e5915292d7cbdcca
Mirela Mileva <mirela.tsvetanova2@gmail.com>
2020-11-08 13:49:25 +0000

    Add Explicit Interfaces Problem

---
+++ InterfacesAndAbstraction/ExplicitInterfaces/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace ExplicitInterfaces
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string input;
+            while ((input = Console.ReadLine()) != "End")
+            {
+                var inputInfo = input
+                    .Split()
+                    .ToArray();
+
+                string name = inputInfo[0];
+                string country = inputInfo[1];
+                int age = int.Parse(inputInfo[2]);
+                Citizen citizen = new Citizen(name,country, age);
+                IPerson citizenName = citizen;
+                IResident residentName = citizen;
+                Console.WriteLine(citizenName.GetName());
+                Console.WriteLine(residentName.GetName());
+            }
+        }
+    }
+}
