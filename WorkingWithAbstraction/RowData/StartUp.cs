81cc8251eb85c9a2d8e7dffadbdbcbea6aff69b9
Mirela Mileva <mirela.tsvetanova2@gmail.com>
2020-11-10 00:21:04 +0000

    Add Row Data Refactoring

---
+++ WorkingWithAbstraction/RowData/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RowData
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            ProgramEngine engine = new ProgramEngine();
+            engine.Run();
+        }
+    }
+}
