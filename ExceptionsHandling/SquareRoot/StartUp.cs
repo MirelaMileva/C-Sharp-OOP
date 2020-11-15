dba81f2f66861b3f02dcefe77a1ad858dbdb9b83
Mirela Mileva <mirela.tsvetanova2@gmail.com>
2020-11-15 08:02:04 +0000

    Add Square Root Problem

---
+++ ExceptionsHandling/SquareRoot/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SquareRoot
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                int num = int.Parse(Console.ReadLine());
+                Console.WriteLine(Sqrt(num));
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.Error.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
+
+        }
+        public static double Sqrt(int value)
+        {
+            if (value < 0)
+            {
+                throw new System.ArgumentOutOfRangeException("value",
+                  "Sqrt for negative numbers is undefined!");
+            }
+
+            return Math.Sqrt(value);
+        }
+
+    }
+}
