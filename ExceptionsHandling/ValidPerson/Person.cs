cfd4a345069e2e1bc893c20ca4697705a6edeadd
Mirela Mileva <mirela.tsvetanova2@gmail.com>
2020-11-15 08:36:02 +0000

    Add Valid Person Problem

---
+++ ExceptionsHandling/ValidPerson/Person.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ValidPerson
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        public Person(string firstName, string lastName, int age)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+        }
+
+        public string FirstName 
+        {
+            get
+            {
+                return this.firstName;
+            }
+            private set
+            {
+                if (String.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("value", "The first name cannot be null or empty!");
+                }
+
+                this.firstName = value;
+            }
+        }
+        public string LastName
+        { 
+            get
+            {
+                return this.lastName;
+            }
+            private set
+            {
+                if (String.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("value", "The first name cannot be null or empty!");
+                }
+
+                this.lastName = value;
+            }
+        }
+        public int Age 
+        { 
+            get
+            {
+                return this.age;
+            }
+            private set
+            {
+                if (value < 0 || 120 < value)
+                {
+                    throw new ArgumentOutOfRangeException("value", "Age should be in the range [0..... 120].");
+                }
+
+                this.age = value;
+            }
+        }
+    }
+}
---
+++ ExceptionsHandling/ValidPerson/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ValidPerson
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Person pesho = new Person("Pesho", "Petrov", 20);
+
+                Person noName = new Person(string.Empty, "Todorova", 5);
+
+                Person noLastName = new Person("Gosho", null, 30);
+
+                Person negativeAge = new Person("Mimi", "Ivanova", -50);
+
+                Person tooOldForThisProgram = new Person("Gosho", "Goshev", 125);
+
+            }
+            catch (ArgumentNullException ane)
+            {
+                Console.WriteLine(ane.Message);
+            }
+            catch(ArgumentOutOfRangeException aore)
+            {
+                Console.WriteLine(aore.Message);
+            }
+        }
+    }
+}
