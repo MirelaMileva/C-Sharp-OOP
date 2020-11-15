01d890cf7aae6e68807846e98ee8f7cb77849a88
Mirela Mileva <mirela.tsvetanova2@gmail.com>
2020-11-15 08:19:47 +0000

    Add Convert To Double Problem

---
+++ ExceptionsHandling/ConvertToDouble/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConvertToDouble
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            try
+            {
+                double number = Convert.ToDouble(input);
+                Console.WriteLine(number);
+            }
+            catch (FormatException fe)
+            {
+                Console.WriteLine(fe.Message);
+            }
+            catch(OverflowException ofe)
+            {
+                Console.WriteLine(ofe.Message);
+            }
+        }
+    }
+}
