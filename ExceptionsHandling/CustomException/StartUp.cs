992f421a448a3995a62931456569a8386c68b6e4
Mirela Mileva <mirela.tsvetanova2@gmail.com>
2020-11-15 09:11:13 +0000

    Add Custom Exception Problem

---
+++ ExceptionsHandling/CustomException/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CustomException
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Student gincho = new Student("Gin4o", "gincho.gmail.com");
+                Console.WriteLine(gincho);
+            }
+            catch (InvalidPersonNameException ipne)
+            {
+                Console.WriteLine(ipne.Message);
+            }
+        }
+    }
+}
---
+++ ExceptionsHandling/CustomException/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CustomException
+{
+    public class Student
+    {
+        private string name;
+        private string email;
+
+        public Student(string name, string email)
+        {
+            this.Name = name;
+            this.Email = email;
+        }
+
+        public string Name 
+        { 
+            get
+            {
+                return this.name;
+            }
+            private set
+            {
+                if (!(value.All(ch => Char.IsDigit(ch))))
+                {
+                    throw new InvalidPersonNameException("The name cannot contains digits or special characters!");
+                }
+
+                this.name = value;
+            }
+        }
+        public string Email { get; private set; }
+    }
+}
