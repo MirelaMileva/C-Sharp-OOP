129d5ddf1ca4fda8569b096f16c81775af38ddc2
Mirela Mileva <mirela.tsvetanova2@gmail.com>
2020-11-15 08:11:53 +0000

    Add Fixing Vol2

---
+++ ExceptionsHandling/FixingVol2/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FixingVol2
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int num1 = 30;
+            int num2 = 60;
+
+            try
+            {
+                byte result = Convert.ToByte(num1 * num2);
+                Console.WriteLine("{0} x {1} = {2}", num1, num2, result);
+                Console.WriteLine();
+            }
+            catch (OverflowException ofe)
+            {
+                Console.WriteLine(ofe.Message);
+            }
+
+        }
+    }
+}
