0316de53978e8ebd9f70ed2724dfea076df9630b
Mirela Mileva <mirela.tsvetanova2@gmail.com>
2020-11-26 17:42:47 +0000

    Add Changes

--- ReflectionAndAttributesLab/01.Stealer/Spy.cs
+++ ReflectionAndAttributesLab/01.Stealer/Spy.cs
@@ -75,5 +75,26 @@ namespace Stealer
 
             return sb.ToString().TrimEnd();
         }
+
+        public string CollectGettersAndSetters(string investigatedClass)
+        {
+            Type classType = Type.GetType(investigatedClass);
+
+            MethodInfo[] classMethods = classType.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (MethodInfo method in classMethods.Where(m => m.Name.StartsWith("get")))
+            {
+                sb.AppendLine($"{method.Name} will return {method.ReturnType}");
+            }
+
+            foreach (MethodInfo methodInfo in classMethods.Where(m => m.Name.StartsWith("set")))
+            {
+                sb.AppendLine($"{methodInfo.Name} will set field of {methodInfo.GetParameters().First().ParameterType}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
     }
 }
