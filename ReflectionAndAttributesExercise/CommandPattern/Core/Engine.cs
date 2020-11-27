b3f245fda03162c2a3ea4c21bfc975a2ea227222
Mirela Mileva <mirela.tsvetanova2@gmail.com>
2020-11-27 11:33:57 +0000

    Add Command Pattern Problem

---
+++ ReflectionAndAttributesExercise/CommandPattern/Core/Engine.cs
@@ -0,0 +1,32 @@
+﻿namespace CommandPattern.Core
+{
+    using System;
+
+    using CommandPattern.Core.Contracts;
+
+    public class Engine : IEngine
+    {
+        private readonly ICommandInterpreter commandInterpreter;
+        public Engine(ICommandInterpreter commandInterpreter)
+        {
+            this.commandInterpreter = commandInterpreter;
+        }
+        public void Run()
+        {
+            while (true)
+            {
+                string args = Console.ReadLine();
+                try
+                {
+                    string result = this.commandInterpreter.Read(args);
+
+                    Console.WriteLine(result);
+                }
+                catch (ArgumentException ae)
+                {
+                    Console.WriteLine(ae.Message);
+                }
+            }
+        }
+    }
+}
