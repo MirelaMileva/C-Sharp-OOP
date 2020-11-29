34b980be0cc59d1fdc1b64ba375fb76e22296640
Mirela Mileva <mirela.tsvetanova2@gmail.com>
2020-11-29 08:51:02 +0000

    Add Changes With Mock

--- UnitTestingLab/DependenciesDemo.Tests/Dragon.Tests.cs
+++ UnitTestingLab/DependenciesDemo.Tests/Dragon.Tests.cs
@@ -2,7 +2,7 @@ namespace DependenciesDemo.Tests
 {
     using NUnit.Framework;
 
-    using DependenciesDemo.Tests.Fakes;
+    using Moq;
 
     [TestFixture]
     public class DragonTests
@@ -11,15 +11,26 @@ namespace DependenciesDemo.Tests
         public void DragonShouldIntroduceItsName()
         {
             const string name = "Drago";
+
             //Arrange
-            var introducer = new FakeIntroducer();
-            var dragon = new Dragon(name, introducer);
+
+            //var introducer = new FakeIntroducer();
+            //var dragon = new Dragon(name, introducer);
+
+            var fakeIntroducer = new Mock<IIntroducer>();
+            var introducedMessage = string.Empty;
+
+            fakeIntroducer
+                .Setup(i => i.Introduce(It.IsAny<string>()))
+                .Callback((string message) => introducedMessage = message);
+
+            var dragon = new Dragon(name, fakeIntroducer.Object);
 
             //Act
             dragon.Introduce();
 
             //Assert
-            Assert.That(introducer.Message, Is.EqualTo($"My name is {name}! I am an ancient..."));
+            Assert.That(introducedMessage, Is.EqualTo($"My name is {name}! I am an ancient..."));
         }
     }
 }
